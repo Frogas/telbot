1d083d05ea46a6cb217128c5b69e2719ec74c1ea
Shikukiza <57547930+Shikukiza@users.noreply.github.com>
2020-10-06 15:14:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ const axios = require('axios');
 
 //var @bot.on(string)
 bot.on('message', (ctx) => {
-    const chatId = ctx.chat.id;
-    const name = ctx.from.first_name;
-    bot.telegram.sendMessage(chatId, 'Welcome ' + name);
+  const chatId = ctx.chat.id;
+  const name = ctx.from.first_name;
+  bot.telegram.sendMessage(chatId, 'Welcome ' + name);
 });
 
 //var @commands = /start
 bot.command('start', ctx => {
-    const chatId = ctx.chat.id;
-    bot.telegram.sendMessage(chatId, 'Choose a menu\n/help - Untuk info tentang bot')
+  const chatId = ctx.chat.id;
+  bot.telegram.sendMessage(chatId, 'Choose a menu\n/help - Untuk info tentang bot')
 });
 
 //var @commands = /help
 bot.command('help', ctx => {
-    const chatId = ctx.chat.id;
-    bot.telegram.sendMessage(chatId, 'Bot layanan chat');
+  const chatId = ctx.chat.id;
+  bot.telegram.sendMessage(chatId, 'Bot layanan chat');
 });
 
 //var @button-keyboard
 bot.command('menu', ctx => {
-    const chatId = ctx.chat.id;
-    bot.telegram.sendMessage(chatId, 'Pilih Button di keyboard anda:', {
-      reply_markup: {
-        keyboard: [
-            [
-              { text: Button 1 },
-              { text: Button 2 }
-            ]
-        ],
-        //boolean
-        resize_keyboard: true, //false
-        one_time_keyboard: false //true
-      }
-    });
+  const chatId = ctx.chat.id;
+  bot.telegram.sendMessage(chatId, 'Pilih Button di keyboard anda:', {
+    reply_markup: {
+      keyboard: [
+        [
+          { text: 'Button 1' },
+          { text: 'Button 2' }
+        ]
+      ],
+      //boolean
+      resize_keyboard: true, //false
+      one_time_keyboard: false //true
+    }
+  });
 });
 
 bot.launch();
